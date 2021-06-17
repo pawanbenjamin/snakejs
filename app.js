aaedce306e79b18063e6e68f205ff5f4d856d0f1
Pawan Benjamin <pawanbenjamin@gmail.com>
2021-06-17 19:13:20 -0400

    clear interval

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 let gridSize = 20;
+let score = 0;
+
+function updateScore(score) {
+  $(".score").text(score);
+}
 
 function makeGrid() {
   for (let i = 0; i < gridSize; i++) {
@@ -22,8 +27,29 @@ let LEFT = [0, -1];
 let RIGHT = [0, 1];
 let DIRECTION = RIGHT;
 
+let fruitCoords = [9, 9];
+
+function renderFruit(fruitCoords) {
+  $(".grid div").removeClass("fruit");
+  $(`#${fruitCoords[0]}-${fruitCoords[1]}`).addClass("fruit");
+}
+
 function move(dir) {
   let newHead = [snake[0][0] + dir[0], snake[0][1] + dir[1]];
+
+  snake.forEach((coord) => {
+    if (newHead[0] === coord[0] && newHead[1] === coord[1]) {
+      $(".grid div").removeClass("snake");
+      $(".grid div").removeClass("fruit");
+      clearInterval(clearInt);
+      score = 0;
+      updateScore(score);
+      snake = [[0, 0]];
+      fruitCoords = [9, 9];
+      render();
+      return;
+    }
+  });
 
   if (newHead[0] < 0) {
     newHead[0] = gridSize - 1;
@@ -38,8 +64,17 @@ function move(dir) {
     newHead[1] = 0;
   }
 
-  snake.pop();
-  snake.unshift(newHead);
+  const [x, y] = fruitCoords;
+  if (newHead[0] === x && newHead[1] === y) {
+    score++;
+    updateScore(score);
+    fruitCoords = setNewFruit();
+    snake.unshift(newHead);
+    renderFruit(fruitCoords);
+  } else {
+    snake.pop();
+    snake.unshift(newHead);
+  }
 }
 
 document.addEventListener("keydown", (e) => {
@@ -58,8 +93,18 @@ document.addEventListener("keydown", (e) => {
 function render() {
   move(DIRECTION);
   renderSnake();
+  renderFruit(fruitCoords);
 }
 
 makeGrid();
+let clearInt = setInterval(render, 100);
 
-// setInterval(render, 100);
+document.getElementById("render").addEventListener("click", () => {
+  render();
+});
+
+function setNewFruit() {
+  let newX = Math.floor(Math.random() * (gridSize - 1));
+  let newY = Math.floor(Math.random() * gridSize);
+  return [newX, newY];
+}
