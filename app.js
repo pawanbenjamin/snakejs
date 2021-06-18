7ca276c2741ffb8999a7f250fd47843c201429f7
pawanbenjamin <pawanbenjamin@gmail.com>
2021-06-18 10:18:50 -0400

    updated solution

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-// Define Our Grid Size
+// Game Data
 let gridSize = 20;
-
-//Define Coordinates of our Direction
+let score = 0;
 let UP = [-1, 0];
 let DOWN = [1, 0];
 let LEFT = [0, -1];
 let RIGHT = [0, 1];
-let DIRECTION = RIGHT;
 
-//Other Variables that we will use
-let score = 0;
-let highScore = 0;
-let fruitCoords = [9, 9];
-let snake = [[0, 0]];
+// Global Variable for storing returned value of setInterval
+let stop;
 
-//setting a global variable which will store the return of setInterval()
-let clearInt;
+// Game State
+let state = {
+  snake: [[0, 0]],
+  DIRECTION: RIGHT,
+  fruitCoords: [9, 9],
+  gameState: "isPlaying",
+};
 
-//Append a new div with coordinates passed into our ID to our grid
+// Place cells with id of grid coordinates x-y in our CSS grid
 function makeGrid() {
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
@@ -26,49 +26,44 @@ function makeGrid() {
   }
 }
 
-//Add the class of active to our snake coordinates
+// Change the div with our snake coordinates to have a black background
 function renderSnake() {
   $(".grid div").removeClass("active");
-  //for each coordinate in our snake array
-  snake.map((coordinate) => {
-    // add the active class to it to color it in
+  state.snake.map((coordinate) => {
     $(`#${coordinate[0]}-${coordinate[1]}`).addClass("active");
   });
 }
 
-//Adds the class of fruit to the fruit coordinates
-function renderFruit(fruitCoords) {
+// Change the div with the passed in coordinates to our fruit
+function renderFruit(coords) {
   $(".grid div").removeClass("fruit");
-  $(`#${fruitCoords[0]}-${fruitCoords[1]}`).addClass("fruit");
+  $(`#${coords[0]}-${coords[1]}`).addClass("fruit");
 }
 
-// Move our snake!
+// What happens when we move
 function move(dir) {
-  //Our new head of the snake will be the old coordinates with the direction added to each
-  //   snake[0][0] is referring to the first nested array's (the head of the snake) first value
-  //   snake [0][1] is referring to the head of our snake's second value
-  //   we're adding the x and y values of the direction to each number and making that our newHead
-  let newHead = [snake[0][0] + dir[0], snake[0][1] + dir[1]];
+  // add our directions coordinates to our snake head to find our new head
+  let newHead = [state.snake[0][0] + dir[0], state.snake[0][1] + dir[1]];
 
-  snake.forEach((coord) => {
-    // if our new head's x and y values are the same as any in our snake array (we collided with ourself)
-    // then game over and we need to reset everything
+  // For each cooridnate of our snake we want to check if our head collided with it
+  state.snake.forEach((coord) => {
+    // If our newHead x and y is equal to our snake coordinates x and y
     if (newHead[0] === coord[0] && newHead[1] === coord[1]) {
-      if (score > highScore) highScore = score;
-      // All of this here resets the global variables
+      //reset all information
       $(".grid div").removeClass("snake");
       $(".grid div").removeClass("fruit");
-      clearInterval(clearInt);
+      newHead = [0, 0];
+      state.snake = [[0, 0]];
+      state.DIRECTION = RIGHT;
+      state.fruitCoords = [9, 9];
       score = 0;
       updateScore(score);
-      snake = [[0, 0]];
-      fruitCoords = [9, 9];
-      render();
-      return;
+      state.gameState = "game-over";
+      $(".game-over").text("GAME OVER");
     }
   });
 
-  // these check if our snake is out of our grid boundary, if so, makes it appear on the opposite end
+  // If our new head is out of our grid bounds, have it show up on the other side
   if (newHead[0] < 0) {
     newHead[0] = gridSize - 1;
   }
@@ -82,65 +77,67 @@ function move(dir) {
     newHead[1] = 0;
   }
 
-  //this pulls each coordinate from our current fruitCoords
-  const [x, y] = fruitCoords;
-  // if both these coordinates overlap with our new snake head, then we've eaten the fruit
+  // Deconstruction of our fruit coordinates
+  const [x, y] = state.fruitCoords;
+  // If our new head have the same coordinates as our fruit
   if (newHead[0] === x && newHead[1] === y) {
+    // Increment score, place newHead at the begning of our snake and place a new fruit
     score++;
     updateScore(score);
-    fruitCoords = setNewFruit();
-    //this adds the new head to the beggining of our snake array
-    snake.unshift(newHead);
-    //this places a new piece of fruit
-    renderFruit(fruitCoords);
+    state.fruitCoords = setNewFruit();
+    state.snake.unshift(newHead);
+    renderFruit(state.fruitCoords);
   } else {
-    //else we remove the tail of our snake because we didnt eat a fruit
-    snake.pop();
-    //and we add our newHead to the beggining of the snake
-    snake.unshift(newHead);
+    // Or else we keep the snake the same length by removing the last element
+    state.snake.pop();
+    // Replacing our head with our newHead coordinates
+    state.snake.unshift(newHead);
   }
 }
 
-//Query the score class, and update it with our global score
+// Generate random coordinates to place our fruit
+function setNewFruit() {
+  let newX = Math.floor(Math.random() * (gridSize - 1));
+  let newY = Math.floor(Math.random() * (gridSize - 1));
+  return [newX, newY];
+}
+
+// Update our score in the DOM
 function updateScore(score) {
   $(".score").text(score);
 }
 
-// generates random fruit coordinates
-function setNewFruit() {
-  let newX = Math.floor(Math.random() * (gridSize - 1));
-  let newY = Math.floor(Math.random() * gridSize);
-  return [newX, newY];
-}
-
-//this needs to happen at a specific interval so the game can start
+// If our game state isPlaying, render our game, if not stop it
 function render() {
-  //move our snake
-  move(DIRECTION);
-  //build our snake
-  renderSnake();
-  //drop our fruit
-  renderFruit(fruitCoords);
+  if (state.gameState === "isPlaying") {
+    renderSnake();
+    move(state.DIRECTION);
+    renderFruit(state.fruitCoords);
+  } else {
+    // Uses the global stop variable and pass it to this clearInterval function
+    clearInterval(stop);
+  }
 }
 
-//invoke make grid to make our grid
-makeGrid();
-
-document.getElementById("render").addEventListener("click", () => {
-  //setting the global to be the return of setInterval, so when it's called on line 61 we kill the game
-  clearInt = setInterval(render, 50);
-});
-
-//listeners for key movements
+// Listen for key presses from the user
 document.addEventListener("keydown", (e) => {
   if (e.code === "ArrowUp") {
-    DIRECTION = UP;
+    state.DIRECTION = UP;
   } else if (e.code === "ArrowRight") {
-    DIRECTION = RIGHT;
+    state.DIRECTION = RIGHT;
   } else if (e.code === "ArrowDown") {
-    DIRECTION = DOWN;
+    state.DIRECTION = DOWN;
   } else if (e.code === "ArrowLeft") {
-    DIRECTION = LEFT;
+    state.DIRECTION = LEFT;
   }
-  console.log(DIRECTION);
 });
+
+// Click the button to start the game
+document.getElementById("render").addEventListener("click", () => {
+  state.gameState = "isPlaying";
+  $(".game-over").text("Snakey Sssnake");
+  stop = setInterval(render, 100);
+});
+
+// Call our makeGrid function and place all of our cells
+makeGrid();
